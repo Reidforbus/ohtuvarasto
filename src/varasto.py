783c22d5eafd0b8b56609433299c77061cf5df25
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-11-07 21:45:18 +0200

    breaking pylint checks

--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Erillistä kenttää ei tarvita
+    # huom: ominaisuus voidaan myös laskea. Erillistä kenttää ei tarvita. Nyt tämä rivi on liian pitkä
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
